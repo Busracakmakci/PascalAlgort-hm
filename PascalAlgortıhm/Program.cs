0d51bbf2c2efaeb90fa7fa6b98550203db0e87b1
Büşra Çakmakcı <busracakmakci95@gmail.com>
2023-09-15 16:52:26 +0300

    Add project files.

---
+++ PascalAlgortıhm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 6; 
+
+            for (int i = 0; i < n; i++)
+            {
+               
+                for (int j = 0; j < n - i; j++)
+                {
+                    Console.Write("  ");
+                }
+
+
+                int number =1; 
+
+                
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(number + "   ");
+                    number = number * (i - j) / (j + 1); 
+                }
+
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
+
